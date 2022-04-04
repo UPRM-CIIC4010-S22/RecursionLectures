25876e5ae2b5f025638fc31782128c14458b12fc
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2022-04-04 17:12:29 -0400

    Added iter and rec factorials

--- Recursion.cpp
+++ Recursion.cpp
@@ -2,8 +2,38 @@
 
 using namespace std;
 
+long iterFactorial(int n) {
+
+    long product = 1;
+    for (int i=1; i<=n; i++) {
+        product *= i;
+    }
+    return product;
+}
+
+long recFactorial(int n) {
+
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * recFactorial(n - 1);
+    }
+}
+
 int main() {
 
     cout << "Hello World" << endl;
+
+    for (int i=0; i<=10; i++) {
+
+        cout << "iterFactorial(" << i << ") = " << iterFactorial(i) << endl;
+
+    }
+
+    for (int i=0; i<=10; i++) {
+
+        cout << "recFactorial(" << i << ") = " << recFactorial(i) << endl;
+
+    }
     
 }
